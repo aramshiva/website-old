5018d25eab4ecdd7c25f50657a373628492da028
Aram <me@aram.sh>
2025-01-29 16:28:47 -0800

    Remove unused Fm component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Fm from "@/components/fm";
 import Nav from "@/components/nav";
 import { SessionProvider } from "next-auth/react";
 import SpotifyWidget from "@/components/music";
