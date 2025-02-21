52bc064c264393e34e30beabea71da641bce79c5
Aram <me@aram.sh>
2025-02-20 19:46:38 -0800

    add webring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Index from "@/markdown/index.mdx";
+import Script from "next/script";
 export default function Page() {
   return (
     <>
       <Index />
+      <div id="elr">
+        <Script
+          type="text/javascript"
+          src="https://w.elr.sh/onionring-variables.js"
+        />
+        <Script
+          type="text/javascript"
+          src="https://w.elr.sh/onionring-widget.js"
+        />
+      </div>
     </>
   );
 }
